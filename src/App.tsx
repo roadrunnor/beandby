aa1c2f944619c1b14392eff0a7511aa11168d436
sylvain breton <sylvain.breton@gmail.com>
2024-05-23 15:15:04 -0400

    fixed logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./App.css";
-import tulipeNoBg from "./assets/tulipe_no_bg.svg"; 
+import tulipeNoBg from "./assets/tulipe_no_bg.svg";
 
 const App: React.FC = () => {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h1>B&BY</h1>
-				<p>Coming soon</p>
+				<div className="logo-container">
+					<img src={tulipeNoBg} alt="Logo" className="logo" />
+					<div className="text-container">
+						<h1>B&BY .</h1>
+						<p>Coming soon</p>
+					</div>
+				</div>
 			</header>
 		</div>
 	);
