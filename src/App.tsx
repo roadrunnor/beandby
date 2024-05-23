3c4a8ca553d9d22619be0a9a8396ae8503bbd566
sylvain breton <sylvain.breton@gmail.com>
2024-05-23 13:41:06 -0400

    Created first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import tulipeNoBg from "./assets/tulipe_no_bg.svg"; 
+
+const App: React.FC = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>B&BY</h1>
+				<p>Coming soon</p>
+			</header>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/Letters.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Letters: React.FC = () => {
+  return <h1>B&BY.</h1>;
+};
+
+export default Letters;
